c08e4b02b771df96ed1cb0c65ff68029dc40538a
Saeedul <karimsaeedul476@gmail.com>
2026-05-16 17:35:27 +0600

    finalized  Our Services section

--- src/pages/Home/OurServices/OurServices.jsx
+++ src/pages/Home/OurServices/OurServices.jsx
@@ -5,11 +5,11 @@ import OurServicesCard from './OurServicesCard';
 const OurServices = () => {
     return (
         <section className="bg-[#03373D] py-20 my-7 md:my-10 lg:my-15 rounded-3xl">
-            <div className="w-11/12 mx auto">
-                <h2 className="text-4xl font-bold text-white mb-12">
+            <div className="w-11/12 mx-auto">
+                <h2 className="text-4xl font-bold text-white text-center">
                     Our Services
                 </h2>
-                <p className="text-[#DADADA]">
+                <p className="text-[#DADADA] w-full md:w-8/12 mx-auto text-center leading-7 my-6">
                     Enjoy fast, reliable parcel delivery with real-time tracking and zero hassle. From personal packages to business shipments — we deliver on time, every time.
                 </p>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
--- src/pages/Home/OurServices/OurServicesCard.jsx
+++ src/pages/Home/OurServices/OurServicesCard.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const OurServicesCard = ({ item }) => {
     return (
-        <div className="bg-white rounded-2xl p-8">
-            <img
-                src={item.icon}
-                alt={item.title}
-                className="w-12 mb-5"
-            />
+        <div className="bg-white rounded-2xl p-8 text-center">
+            <div className="flex justify-center">
+                <img
+                    src={item.icon}
+                    alt={item.title}
+                    className="w-12 mb-5"
+                />
+            </div>
             <h3 className="text-xl font-semibold mb-4">
                 {item.title}
             </h3>
