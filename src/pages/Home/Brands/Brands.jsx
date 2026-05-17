07dd26a425a515d37acb7072c121ff0da995f8b0
Saeedul <karimsaeedul476@gmail.com>
2026-05-18 01:13:03 +0600

    finalized brands section

--- src/pages/Home/Brands/Brands.jsx
+++ src/pages/Home/Brands/Brands.jsx
@@ -8,6 +8,7 @@ import moonstar from '../../../assets/brands/moonstar.png';
 import randstad from '../../../assets/brands/randstad.png';
 import star from '../../../assets/brands/star.png';
 import start_people from '../../../assets/brands/start_people.png';
+import { Autoplay } from 'swiper/modules';
 
 const brandLogos = [amazon, amazon_vector, casio, moonstar, randstad, star, start_people];
 
@@ -19,6 +20,11 @@ const Brands = () => {
             centeredSlides={true}
             spaceBetween={30}
             grabCursor={true}
+            modules={[Autoplay]}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}
         >
             {
                 brandLogos.map((logo, index) => <SwiperSlide key={index}>
